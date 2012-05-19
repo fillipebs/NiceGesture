2736871246a2760ccaec3a07a6d35425e5387abd
Fillipe Silva <fillipe.bs@gmail.com>
2012-05-19 13:29:01 -0300

    Primeiro Commit

---
+++ src/ufrj/dcc/agil/MoveGestureActivity.java
@@ -0,0 +1,13 @@
+package ufrj.dcc.agil;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MoveGestureActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
