44f78c34d3a3b6bcfe6404c344dd00e52229b33c
Renan Francisco Souza <renan-francisco@hotmail.com>
2012-06-02 12:00:22 -0300

    atalho para app implementado faltando testes

--- NiceGesture/src/ufrj/dcc/agil/MoveGestureActivity.java
+++ NiceGesture/src/ufrj/dcc/agil/MoveGestureActivity.java
@@ -1,7 +1,14 @@
 package ufrj.dcc.agil;
 
 import android.app.Activity;
+import android.app.KeyguardManager;
+import android.app.KeyguardManager.KeyguardLock;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MoveGestureActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,9 +16,19 @@ public class MoveGestureActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	 abreAplicativo("com.android.calculator2", "com.android.calculator2.Calculator");
+            	 
+            }	
+        });    
     }
     
-    public int a(){
-    	return 1;
-    }
+    public void abreAplicativo(String caminho, String caminhoNome) {
+		Intent i = new Intent();
+         i.setClassName(caminho,caminhoNome);
+         startActivity(i);
+	}
 }
--- NiceGestureTest/src/ufrj/dcc/agil/test/TesteClasse.java
+++ NiceGestureTest/src/ufrj/dcc/agil/test/TesteClasse.java
@@ -1,7 +1,15 @@
 package ufrj.dcc.agil.test;
 
+import java.util.Iterator;
+import java.util.List;
+
 import ufrj.dcc.agil.MoveGestureActivity;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 
 public class TesteClasse extends ActivityInstrumentationTestCase2<MoveGestureActivity> {
 
@@ -18,9 +26,47 @@ public class TesteClasse extends ActivityInstrumentationTestCase2<MoveGestureAct
 		
 	}
 	
-	public void testNomeAplicacao() {
-		assertEquals(demo.a(),0);
+	public void testNomeNossaAplicacao() {
+		ActivityManager am = (ActivityManager)demo.getSystemService(demo.ACTIVITY_SERVICE);
+		List l = am.getRunningAppProcesses();
+		PackageManager pm = demo.getPackageManager();
+		ActivityManager.RunningAppProcessInfo info2 = (ActivityManager.RunningAppProcessInfo)l.get(0);
+		try {
+			String nomeAplicativo = pm.getApplicationLabel(pm.getApplicationInfo(info2.processName, PackageManager.GET_META_DATA)).toString();
+			assertEquals(nomeAplicativo,"NiceGesture");
+		} catch (NameNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 
+	public void testAbreAplicacao() {
+		//demo.abreAplicativo("com.android.calculator2", "com.android.calculator2.Calculator");
+		/*ActivityManager am = (ActivityManager)demo.getSystemService(demo.ACTIVITY_SERVICE);
+		List l = am.getRunningAppProcesses();
+		Iterator i = l.iterator();
+		PackageManager pm = demo.getPackageManager();
+		ActivityManager.RunningAppProcessInfo info2 = (ActivityManager.RunningAppProcessInfo)l.get(0);
+		
+		try {
+			String nomeAplicativo = pm.getApplicationLabel(pm.getApplicationInfo(info2.processName, PackageManager.GET_META_DATA)).toString();
+			Log.e("NOME", nomeAplicativo);
+			//assertEquals(nomeAplicativo,"NiceGesture");
+		} catch (NameNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		while(i.hasNext()){
+			ActivityManager.RunningAppProcessInfo info3 = (ActivityManager.RunningAppProcessInfo)(i.next());
+			try {
+				String nomeAplicativo2 = pm.getApplicationLabel(pm.getApplicationInfo(info3.processName, PackageManager.GET_META_DATA)).toString();
+				Log.e("lista", nomeAplicativo2);
+			} catch (NameNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}*/
+	}
 	
 }
