045df2aedd6e20f19705f2f532f6661cb885c36a
Guilherme Bruzzi <guibruzzi@gmail.com>
2012-06-02 15:40:25 -0300

    Parte de testes funcionando

--- NiceGesture/src/ufrj/dcc/agil/MoveGestureActivity.java
+++ NiceGesture/src/ufrj/dcc/agil/MoveGestureActivity.java
@@ -1,14 +1,8 @@
 package ufrj.dcc.agil;
 
 import android.app.Activity;
-import android.app.KeyguardManager;
-import android.app.KeyguardManager.KeyguardLock;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MoveGestureActivity extends Activity {
     /** Called when the activity is first created. */
@@ -17,13 +11,13 @@ public class MoveGestureActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        final Button button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	 abreAplicativo("com.android.calculator2", "com.android.calculator2.Calculator");
-            	 
-            }	
-        });    
+        abreAplicativo("com.android.calculator2", "com.android.calculator2.Calculator");
+		// final Button button = (Button) findViewById(R.id.button1);
+		// button.setOnClickListener(new View.OnClickListener() {
+		// public void onClick(View v) {
+		//
+		// }
+		// });
     }
     
     public void abreAplicativo(String caminho, String caminhoNome) {
--- NiceGestureTest/src/ufrj/dcc/agil/test/TesteClasse.java
+++ NiceGestureTest/src/ufrj/dcc/agil/test/TesteClasse.java
@@ -1,72 +1,58 @@
 package ufrj.dcc.agil.test;
 
-import java.util.Iterator;
 import java.util.List;
 
 import ufrj.dcc.agil.MoveGestureActivity;
-import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.test.ActivityInstrumentationTestCase2;
-import android.util.Log;
 
-public class TesteClasse extends ActivityInstrumentationTestCase2<MoveGestureActivity> {
+public class TesteClasse extends
+		ActivityInstrumentationTestCase2<MoveGestureActivity> {
 
-	
 	MoveGestureActivity demo;
-	
+	ActivityManager manager;
+
 	public TesteClasse() {
 		super("ufrj.dcc.agil", MoveGestureActivity.class);
 	}
-	
+
 	@Override
 	protected void setUp() throws Exception {
 		demo = this.getActivity();
-		
-	}
-	
-	public void testNomeNossaAplicacao() {
-		ActivityManager am = (ActivityManager)demo.getSystemService(demo.ACTIVITY_SERVICE);
-		List l = am.getRunningAppProcesses();
-		PackageManager pm = demo.getPackageManager();
-		ActivityManager.RunningAppProcessInfo info2 = (ActivityManager.RunningAppProcessInfo)l.get(0);
-		try {
-			String nomeAplicativo = pm.getApplicationLabel(pm.getApplicationInfo(info2.processName, PackageManager.GET_META_DATA)).toString();
-			assertEquals(nomeAplicativo,"NiceGesture");
-		} catch (NameNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		manager = (ActivityManager) demo
+				.getSystemService(Context.ACTIVITY_SERVICE);
+
 	}
 
-	public void testAbreAplicacao() {
-		//demo.abreAplicativo("com.android.calculator2", "com.android.calculator2.Calculator");
-		/*ActivityManager am = (ActivityManager)demo.getSystemService(demo.ACTIVITY_SERVICE);
-		List l = am.getRunningAppProcesses();
-		Iterator i = l.iterator();
-		PackageManager pm = demo.getPackageManager();
-		ActivityManager.RunningAppProcessInfo info2 = (ActivityManager.RunningAppProcessInfo)l.get(0);
+	public void testNomeNossaAplicacao() throws NameNotFoundException {
+		PackageManager packageManager = demo.getPackageManager();
+		RunningAppProcessInfo process = getProcessoVisivel();
 		
-		try {
-			String nomeAplicativo = pm.getApplicationLabel(pm.getApplicationInfo(info2.processName, PackageManager.GET_META_DATA)).toString();
-			Log.e("NOME", nomeAplicativo);
-			//assertEquals(nomeAplicativo,"NiceGesture");
-		} catch (NameNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		while(i.hasNext()){
-			ActivityManager.RunningAppProcessInfo info3 = (ActivityManager.RunningAppProcessInfo)(i.next());
-			try {
-				String nomeAplicativo2 = pm.getApplicationLabel(pm.getApplicationInfo(info3.processName, PackageManager.GET_META_DATA)).toString();
-				Log.e("lista", nomeAplicativo2);
-			} catch (NameNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}*/
+
+		String processLabel = (packageManager
+				.getApplicationLabel(packageManager.getApplicationInfo(
+						process.processName, PackageManager.GET_META_DATA)))
+				.toString();
+		assertEquals("Calculator", processLabel);
 	}
-	
+
+	private RunningAppProcessInfo getProcessoVisivel() {
+		List<ActivityManager.RunningAppProcessInfo> processes = manager
+				.getRunningAppProcesses();
+
+		for (ActivityManager.RunningAppProcessInfo process : processes) {
+			// Take a look at the IMPORTANCE_VISIBLE property as well in the
+			// link provided at the bottom
+			if (process.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+				return process;
+			}
+		}
+
+		return null;
+	}
+
 }
